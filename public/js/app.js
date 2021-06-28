2aad43e875a82d7cd46e5c976d2304724de931b9
deepukbn <k.bdeepu471999@gmail.com>
2021-06-29 00:22:24 +0530

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,16 +12,14 @@ weatherForm.addEventListener("submit", (e) => {
 
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = "Location:" + data.location;
-          messageTwo.textContent = "forecast:" + data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = "Location:" + data.location;
+        messageTwo.textContent = "forecast:" + data.forecast;
+      }
+    });
+  });
 });
